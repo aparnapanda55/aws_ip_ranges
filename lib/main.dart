e533533e3436411839be2543fa062e2dd8f93a9a
Aparna Panda <aparnapanda55@gmail.com>
2022-01-18 18:10:30 +0530

    created column with blank dropdowns

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,65 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final data = {
+  'us-east-1': {
+    'EC2': [
+      '13.34.37.64/27',
+      '3.5.140.0/22',
+    ],
+    'CODEBUILD': [
+      '14.34.37.63/27',
+      '30.5.140.3/24',
+    ]
+  },
+  'us-west-2': {
+    'EC2': [
+      '13.34.37.64/27',
+      '3.5.140.0/22',
+    ],
+    'CODEBUILD': [
+      '14.34.37.63/27',
+      '30.5.140.3/24',
+    ],
+    'ROUTE53_RESOLVER': [
+      '99.79.20.224/27',
+    ]
+  }
+};
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AWS IP Ranges'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: DropdownButton<String>(
+              items: [],
+              onChanged: (value) {},
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: DropdownButton<String>(
+              items: [],
+              onChanged: (value) {},
+            ),
+          ),
+          Expanded(
+            flex: 8,
+            child: ListView(
+              children: [],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
